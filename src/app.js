f19d68a705a98307fff76bf60ae8651df220b218
Tgonz089 <tristangonzales8936@gmail.com>
2022-09-13 00:55:16 +0000

    started domain name project. Issue with results.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let generateDomain = () => {
+    let domainArray = [];
+    let pronoun = ["the", "our"];
+    let adj = [
+      "great",
+      "big",
+      "aweletsome",
+      "amazing",
+      "only",
+      "mysterious",
+      "lonely",
+      "strong",
+      "devious",
+      "last",
+      "small",
+      "sleepy",
+    ];
+    let noun = [
+      "jogger",
+      "racoon",
+      "dog",
+      "llama",
+      "cat",
+      "gamer",
+      "programmer",
+    ];
+    let tld = [".com", ".org", ".net", "io", "us"];
+
+    for (i of pronoun) {
+      for (x of adj) {
+        for (y of noun) {
+          for (z of tld) {
+            domainArray.push(pronoun[i] + adj[x] + noun[y] + tld[z]);
+          }
+        }
+      }
+    }
+    return domainArray;
+  };
+  document.querySelector("#domain").innerHTML = generateDomain();
 };
