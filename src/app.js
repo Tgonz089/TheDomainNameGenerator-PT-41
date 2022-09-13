f37ce5fe0819bb6032636e18769ef4a64e24ef50
Tgonz089 <tristangonzales8936@gmail.com>
2022-09-13 01:19:00 +0000

    Fixed issue with displaying results.

--- src/app.js
+++ src/app.js
@@ -9,43 +9,21 @@ window.onload = function() {
   //write your code here
 
   let generateDomain = () => {
-    let domainArray = [];
     let pronoun = ["the", "our"];
-    let adj = [
-      "great",
-      "big",
-      "aweletsome",
-      "amazing",
-      "only",
-      "mysterious",
-      "lonely",
-      "strong",
-      "devious",
-      "last",
-      "small",
-      "sleepy",
-    ];
-    let noun = [
-      "jogger",
-      "racoon",
-      "dog",
-      "llama",
-      "cat",
-      "gamer",
-      "programmer",
-    ];
+    let adj = ["great", "big", "aweletsome", "amazing"];
+    let noun = ["jogger", "racoon", "dog"];
     let tld = [".com", ".org", ".net", "io", "us"];
-
-    for (i of pronoun) {
-      for (x of adj) {
-        for (y of noun) {
-          for (z of tld) {
+    let domainArray = [];
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let x = 0; x < adj.length; x++) {
+        for (let y = 0; y < noun.length; y++) {
+          for (let z = 0; z < tld.length; z++) {
             domainArray.push(pronoun[i] + adj[x] + noun[y] + tld[z]);
           }
         }
       }
     }
-    return domainArray;
+    return domainArray + " ";
   };
   document.querySelector("#domain").innerHTML = generateDomain();
 };
